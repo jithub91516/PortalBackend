ad777823ce6ff24bc747cd999b6de936c9582792
jithub91516 <cyb91516@gmail.com>
2023-12-15 14:18:26 +0900

    oracle service log commit temp

--- ahnlab-portal/src/main/java/com/example/ahnlabportal/OracleUserCRUDTest/OracleService.java
+++ ahnlab-portal/src/main/java/com/example/ahnlabportal/OracleUserCRUDTest/OracleService.java
@@ -8,10 +8,14 @@ import org.springframework.core.ResolvableType;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -42,6 +46,42 @@ public class OracleService {
         reVal = Integer.parseInt(Long.toString(time.getTime() / 1000));
 
         return reVal;
+    }
+
+    public BufferedInputStream exportTest(Map<String, Object> hit) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+        Map<String, String> fieldMap = Stream.of(LogFieldType.values()).collect(Collectors.toMap(LogFieldType::getLogFieldElasticSearch, LogFieldType::getLogFieldDesc));
+        writer.write(String.join(","), fieldMap.values());
+        writer.newLine();
+        Locale lang = Locale.KOREA;
+
+        String line = fieldMap.entrySet().stream().map(
+                entry -> {
+                    Object value = hit.get(entry.getKey());
+                    String formattedValue = switch (entry.getKey()) {
+                        case "1" -> "test1";
+                        case "2" -> "test2";
+                        case "3" -> "test3";
+                    };
+                    return formattedValue;
+                }
+        ).collect(Collectors.joining("~"));
+
+        writer.write(line);
+        writer.newLine();
+
+        writer.flush();
+
+
+        if(wafErr.contains("blind")){
+            if(logType.contains("2")){
+                //I want to pop up "3" if "3" is in logType
+            }else{
+                logType = logType.stream().map(s -> "3".equals(s) ? "2" : s).collect(Collectors.toList());
+            }
+        }
+        return new BufferedInputStream(new ByteArrayInputStream(outputStream.toByteArray()));
     }
 
 }
